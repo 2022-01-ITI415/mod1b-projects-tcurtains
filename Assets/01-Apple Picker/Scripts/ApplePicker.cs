18ecd95e957f067c95b466d0ab4cdc4cad4e6522
tcurtains <tcurtis727@outlook.com>
2022-02-01 19:10:49 -0500

    Remove baskets
    baskets removed when apples are missed

--- Assets/01-Apple Picker/Scripts/ApplePicker.cs
+++ Assets/01-Apple Picker/Scripts/ApplePicker.cs
@@ -1,7 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.SceneManagement;
 public class ApplePicker : MonoBehaviour
 {
     [Header("Set in Inspector")]
@@ -9,19 +9,19 @@ public class ApplePicker : MonoBehaviour
     public int numBaskets = 3;
     public float basketBottomY = -14f;
     public float basketSpacingY = 2f;
+    public List<GameObject> basketList;
 
     // Start is called before the first frame update
     void Start()
     {
+        basketList = new List<GameObject>();
         for (int i = 0; i<numBaskets; i++) 
         {
             GameObject tBasketGo = Instantiate<GameObject>(basketPrefab);
-
             Vector3 pos = Vector3.zero;
-
             pos.y = basketBottomY + (basketSpacingY * i);
-
             tBasketGo.transform.position = pos;
+            basketList.Add(tBasketGo);
         }
     }
 
@@ -33,6 +33,15 @@ public class ApplePicker : MonoBehaviour
         {
             Destroy(tGo);
         }
+
+        // destroy one basket
+        //get index of last basket in basketList
+        int basketIndex = basketList.Count-1;
+        // get ref to Basket GameObject
+        GameObject tBasketGo = basketList[basketIndex];
+        // remove basket from list and destroy GameObject
+        basketList.RemoveAt(basketIndex);
+        Destroy(tBasketGo);
     }
 
     // Update is called once per frame

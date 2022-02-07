365699f230682ee01ef51f4f4b8d507657f5e06b
tcurtains <tcurtis727@outlook.com>
2022-02-07 16:53:00 -0500

    activate projectile

--- Assets/02-Mission Demolition/Scripts/Slingshot.cs
+++ Assets/02-Mission Demolition/Scripts/Slingshot.cs
@@ -4,13 +4,23 @@ using UnityEngine;
 
 public class Slingshot : MonoBehaviour
 {
+    // fields set in inspector pane
+    [Header("Set in Inspector")]
+    public GameObject prefabProjectile;
+
+    // fields set dynamically
+    [Header("Set Dynamically")]
     public GameObject launchPoint;
+    public Vector3 launchPos;
+    public GameObject projectile;
+    public bool aimingMode;
 
     void Awake() 
     {
         Transform launchPointTrans = transform.Find("LaunchPoint");
         launchPoint = launchPointTrans.gameObject;
         launchPoint.SetActive(false);
+        launchPos = launchPointTrans.position;
     }
     void OnMouseEnter() 
     {
@@ -22,6 +32,18 @@ public class Slingshot : MonoBehaviour
     {
         //print("Slingshot:OneMouseExit()");
         launchPoint.SetActive(false);
+    }
+
+    void OnMouseDown() 
+    {
+        //player pressed mouse button while over slingshot
+        aimingMode = true;
+        // instantiate projectile
+        projectile = Instantiate(prefabProjectile) as GameObject;
+        //Starts it at the launchPoint
+        projectile.transform.position = launchPos;
+        // set to isKinematic for now
+        projectile.GetComponent<Rigidbody>().isKinematic = true;
     }
 
     // Start is called before the first frame update

15950612bd096478ca0ad7acd4c52c657f1dd776
tcurtains <tcurtis727@outlook.com>
2022-02-07 16:26:32 -0500

    activate halo on ss

---
+++ Assets/02-Mission Demolition/Scripts/Slingshot.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Slingshot : MonoBehaviour
+{
+    public GameObject launchPoint;
+
+    void Awake() 
+    {
+        Transform launchPointTrans = transform.Find("LaunchPoint");
+        launchPoint = launchPointTrans.gameObject;
+        launchPoint.SetActive(false);
+    }
+    void OnMouseEnter() 
+    {
+       //print("Slingshot:OnMouseEnter()");
+       launchPoint.SetActive(true);
+    }
+
+    void OnMouseExit() 
+    {
+        //print("Slingshot:OneMouseExit()");
+        launchPoint.SetActive(false);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

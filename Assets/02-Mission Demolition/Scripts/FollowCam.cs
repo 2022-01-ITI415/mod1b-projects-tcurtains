ee21795efb5842888057e0805f44a0883887ab46
tcurtains <tcurtis727@outlook.com>
2022-02-08 13:31:17 -0500

    return to shoot

--- Assets/02-Mission Demolition/Scripts/FollowCam.cs
+++ Assets/02-Mission Demolition/Scripts/FollowCam.cs
@@ -18,10 +18,32 @@ public class FollowCam : MonoBehaviour
 
     void FixedUpdate() {
         // if there's only line following an, it does not need braces
-        if (POI == null) return; // return if there is no poi
+        //if (POI == null) return; // return if there is no poi
 
         // get position of poi
-        Vector3 destination = POI.transform.position;
+        //Vector3 destination = POI.transform.position;
+
+        Vector3 destination;
+        // if there is no POI, return to p:(0,0,0)
+        if (POI == null) {
+            destination = Vector3.zero;
+        } else {
+            // get position of POI
+            destination = POI.transform.position;
+        // if POI is a Projectile, check to see if its at rest
+        if (POI.tag == "Projectile") {
+            // if sleeping (that is, not moving)
+            if (POI.GetComponent<Rigidbody>().IsSleeping()) {
+                // return to default view
+                POI = null;
+                // in next update
+                return;
+                }
+
+            }
+        }
+
+
         // limit the X & Y to minimum values
         destination.x = Mathf.Max(minXY.x, destination.x);
         destination.y = Mathf.Max(minXY.y, destination.y);

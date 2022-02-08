7fdb1143879982aa007296ea1e2c0dda31c389cb
tcurtains <tcurtis727@outlook.com>
2022-02-07 22:39:42 -0500

    setting camera
    camera follows projectile correctly

--- Assets/02-Mission Demolition/Scripts/FollowCam.cs
+++ Assets/02-Mission Demolition/Scripts/FollowCam.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class FollowCam : MonoBehaviour
 {
+    [Header("Set in Inspector")]
+    public Vector2 minXY = Vector2.zero;
+    public float easing = 0.05f;
     static public GameObject POI; // static point of interest
 
     [Header("Set Dynamically")]
@@ -19,10 +22,17 @@ public class FollowCam : MonoBehaviour
 
         // get position of poi
         Vector3 destination = POI.transform.position;
+        // limit the X & Y to minimum values
+        destination.x = Mathf.Max(minXY.x, destination.x);
+        destination.y = Mathf.Max(minXY.y, destination.y);
+        //interpolate from current cam position toward destination
+        destination = Vector3.Lerp(transform.position,destination, easing);
         // force destination.z to be camZ to keep cam far away
         destination.z = camZ;
         // set camera to destination
         transform.position = destination;
+        // set orthographicSize of cam to keep Ground in view
+        Camera.main.orthographicSize = destination.y + 10;
     }
 
 

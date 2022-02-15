04317b0a6f14a21db7358e0557413640f2858450
tcurtains <tcurtis727@outlook.com>
2022-02-15 16:59:08 -0500

    my score

--- Assets/03-Prototype1/Scripts/Target.cs
+++ Assets/03-Prototype1/Scripts/Target.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Target : MonoBehaviour {
+    public int score = 0;
     public float speed = 2f;
     public Transform leftPoint;
     public Transform rightPoint;
@@ -29,6 +30,14 @@ public class Target : MonoBehaviour {
         } else {
             rb.MovePosition(rb.position + new Vector2(leftPoint.position.x - speed, 0f) * Time.fixedDeltaTime);
 
+        }
+    }
+
+    void OnCollisionEnter2D (Collider2D col) {
+        if (col.gameObject.tag == "Ball") {
+            score++;
+            speed += 0.25f;
+            Destroy(col.gameObject);
         }
     }
 }
